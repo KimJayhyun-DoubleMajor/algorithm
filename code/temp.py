25736d0b36b2dbdea0d16ed3c7b450a9f670d19e
KimJayhyun <dnlwjem000@gmail.com>
2024-09-17 20:12:50 +0900

    Quick Sort

---
+++ code/temp.py
@@ -0,0 +1,20 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid_idx = len(arr) // 2
+    pivot = arr[mid_idx]
+
+
+arrs = [
+    [8, 7, 6, 5, 4, 3, 2, 1],
+    [-5, -3, 8, 6, -2, 7, -4, 1],
+    [42],
+    [5, 3, 8, 3, 2, 7, 4, 5],
+    [5, 3, 8, 6, 2, 7, 4, 1],
+    [1, 2, 3, 4, 5, 6, 7, 8],
+]
+
+
+for arr in arrs:
+    pass
